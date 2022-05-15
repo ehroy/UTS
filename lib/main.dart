2c81e15feb6b6428c5b0317d9af1240543eea27e
ehroy <sopo78530@gmail.com>
2022-05-14 20:40:40 -0700

    UTS

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+// Nama : Noor Septiyanto
+// NIM : 191240000872
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "UTS PM",
+      debugShowCheckedModeBanner: false,
+      home: MyContent(),
+    );
+  }
+}
+
+class MyContent extends StatelessWidget {
+  const MyContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        color: Colors.grey[200],
+        child: ListView(
+          children: [
+            Image.network(
+              'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Oeschinen_Lake%2C_Kandersteg%2C_Switzerland_%28Unsplash%29.jpg/1184px-Oeschinen_Lake%2C_Kandersteg%2C_Switzerland_%28Unsplash%29.jpg?20170823130402',
+              fit: BoxFit.fitWidth,
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(30, 30, 30, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text('Oeschinen Lake Campground',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        Text('Kandersteg, Switzerland',
+                            style: TextStyle(color: Colors.grey)),
+                      ]),
+                  Row(children: const [
+                    Icon(Icons.star, color: Colors.red),
+                    Text('41'),
+                  ])
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(children: const [
+                    Icon(Icons.call, color: Colors.blue),
+                    Text('CALL', style: TextStyle(color: Colors.blue)),
+                  ]),
+                  Column(children: const [
+                    Icon(Icons.near_me, color: Colors.blue),
+                    Text('ROUTE', style: TextStyle(color: Colors.blue)),
+                  ]),
+                  Column(children: const [
+                    Icon(Icons.share, color: Colors.blue),
+                    Text('SHARE', style: TextStyle(color: Colors.blue)),
+                  ]),
+                ],
+              ),
+            ),
+            Container(
+              height: 300,
+              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+              child: const Text(
+                  "Lake Oeschinen lies at the foot the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degress Cels  ius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run."),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
